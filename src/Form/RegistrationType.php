246c9c3dd9b8e6defec43da09a951400fd8ec282
Olivierbonabal <100704099+Olivierbonabal@users.noreply.github.com>
2022-06-22 11:28:24 +0200

    settingUp_LoginForm

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('password')
+            ->add('fullname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => 2,
+                    'maxlength' => 50,
+                ],
+                'label' => 'Nom & Prénom',
+                'label_attr' => [
+                    
+                ]
+            ])
+            ->add('pseudo')
+            ->add('submit', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
